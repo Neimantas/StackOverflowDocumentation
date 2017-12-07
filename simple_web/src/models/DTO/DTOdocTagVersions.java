4b3f8ee5e05d0f8652790756452820ecec096c3d
Dingau <simas.lukosevicius@gmail.com>
2017-12-07 15:26:44 +0200

    3 DTO Files

---
+++ simple_web/src/models/DTO/DTOdocTagVersions.java
@@ -0,0 +1,13 @@
+package models.DTO;
+
+import java.sql.Date;
+
+public class DTOdocTagVersions {
+	long Id;
+    long DocTagId;
+    String Name;
+    Date CreationDate;
+    Date LastEditDate;
+    long LastEditUserId;
+    Date ReleaseDate;
+}
---
+++ simple_web/src/models/DTO/DTOdogTags.java
@@ -0,0 +1,12 @@
+package models.DTO;
+
+import java.sql.Date;
+
+public class DTOdogTags {
+	long Id;
+    String Tag;
+    String Title;
+    Date CreationDate;
+    long HelloWorldDocTopicId;
+    long TopicCount;
+}
