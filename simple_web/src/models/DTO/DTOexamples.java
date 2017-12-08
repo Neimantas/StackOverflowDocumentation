47c0078425b8e771c14b9ee291ac7457d0cac337
Inga <inga.ziupsniene@gmail.com>
2017-12-08 11:15:17 +0200

    add examples json and DTO model

---
+++ simple_web/src/models/DTO/DTOexamples.java
@@ -0,0 +1,14 @@
+package models.DTO;
+
+public class DTOexamples {
+	long Id;
+    long DocTopicId;
+    String Title;
+    Date CreationDate;
+    Date LastEditDate;
+    long Score;
+    long ContributorCount;
+    String BodyHtml;
+    String BodyMarkdown;
+    boolean IsPinned;
+}
