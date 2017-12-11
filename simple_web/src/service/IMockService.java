82c181d0ecb091f765d8b4c60d22d8794844cf68
Dingau <simas.lukosevicius@gmail.com>
2017-12-11 13:52:55 +0200

    mock example list

--- simple_web/src/service/IMockService.java
+++ simple_web/src/service/IMockService.java
@@ -3,12 +3,14 @@ package service;
 import java.util.List;
 
 import models.java_models.DocTagsVersions;
+import models.java_models.Examples;
 import models.java_models.Topic;
 import models.java_models.mock.TopicMock;
 
 public interface IMockService {
 	public List<DocTagsVersions> getListObject();
 	public List<Topic> getListTopic();
+	public List<Examples> getListExample();
 	
 	public List<DocTagsVersions> getListById(int id);
 	public List<DocTagsVersions> getListByName(String name);
--- simple_web/src/testingMain/TestingMain.java
+++ simple_web/src/testingMain/TestingMain.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import models.java_models.DocTagsVersions;
+import models.java_models.Examples;
 import models.java_models.Topic;
 import service.IMockService;
 import service.MockService;
@@ -21,8 +22,9 @@ public class TestingMain {
 		
 		List<Topic> listTopic= imockService.getListTopic();
 		listTopic.forEach(s->System.out.println(s.id+s.title+s.answer));
-
 		
+		List<Examples> listExamples = imockService.getListExample();
+		listExamples.forEach(s->System.out.println(s.id+s.description+s.docTopicId.title+s.docTopicId.docTagId.title));		
 	}
 
 }
